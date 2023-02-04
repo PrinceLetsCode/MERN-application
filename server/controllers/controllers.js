795de00a2be502aa4d4112083637ba93961e6d69
Prince Kumar <princekumarcodes@gmail.com>
2023-02-04 23:13:48 +0530

    set ups controllers for registring user (using async-await)

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,22 +1,49 @@
 const { User } = require('../models/userSchema');
 
-const registerUser = (req, res) => {
-	const { name, email, phone, work, password, cpassword } = req.body;
-	if (!name || !email || !phone || !work || !password || !cpassword) {
-		return res.status(422).send({ error: "Missing Credentials" });
+const registerUser = async (req, res) => {
+
+	try {
+		const { name, email, phone, work, password, cpassword } = req.body;
+		if (!name || !email || !phone || !work || !password || !cpassword) {
+			return res.status(422).send({ error: "Missing Credentials!" });
+		}
+
+		const userExist = await User.findOne({ email })
+		if (userExist) {
+			return res.status(422).send({ error: "User already exists!" });
+		}
+
+		const user = new User({ name, email, phone, work, password, cpassword });
+		const userRegistered = await User.create(user);
+		if (userRegistered) {
+			res.status(201).send({ message: "User registered successfully!" });
+		}
+		else {
+			res.status(500).send({ error: "User registration failed" });
+		}
+
+	} catch (error) {
+		res.status(500).send({ error });
 	}
 
-	User.findOne({ email })
-		.then((userExist) => {
-			if (userExist) {
-				return res.status(422).send({ error: "User already exists!" });
-			}
+	// USING PROMISE
 
-			const user = new User({ name, email, phone, work, password, cpassword });
-			User.create(user).then(() => {
-				res.status(201).send({ message: "User registered successfully!" });
-			}).catch((err) => res.status(500).send({ error: "User registration failed" }));
-		}).catch(err => res.status(500).send({ error: err }));
+	// const { name, email, phone, work, password, cpassword } = req.body;
+	// if (!name || !email || !phone || !work || !password || !cpassword) {
+	// 	return res.status(422).send({ error: "Missing Credentials" });
+	// }
+
+	// User.findOne({ email })
+	// 	.then((userExist) => {
+	// 		if (userExist) {
+	// 			return res.status(422).send({ error: "User already exists!" });
+	// 		}
+
+	// 		const user = new User({ name, email, phone, work, password, cpassword });
+	// 		User.create(user).then(() => {
+	// 			res.status(201).send({ message: "User registered successfully!" });
+	// 		}).catch((err) => res.status(500).send({ error: "User registration failed" }));
+	// 	}).catch(err => res.status(500).send({ error: err }));
 	
 }
 
